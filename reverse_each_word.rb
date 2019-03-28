282fb9b1e191ba598829a288d96576df36a052e5
Charles Hitt <charles.a.hitt@gmail.com>
2019-03-28 20:10:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
-  array = string.split("")
+  array = string.split(" ")
   new_array = []
   array.each do |string|
     new_array << string.reverse
   end
-  new_array.join("")
+  new_array.join(" ")
 end
   
   reverse_each_word("Hello there, and how are you?")
